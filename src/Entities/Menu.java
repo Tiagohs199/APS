d2729561cb5910367125808db9fde84f0a127eb8
Tiago <tiagohs.199@gmail.com>
2020-05-28 16:56:49 -0300

    final

--- src/Entities/Menu.java
+++ src/Entities/Menu.java
@@ -52,16 +52,12 @@ public class Menu {
 			List<Nota> listCur = data.returnHistoricoCurso(cur);
 			listCur.forEach(nota -> System.out.println(nota));
 			back();
-		case 9 :
-			System.out.print("Digite o Ra do aluno: ");
-			String id1 = sc.next();
-			String id2 = sc.next();
-			System.out.println(data.verifyNota1(id1,id2));
 		case 0:
 			System.exit(0);
 		}
 		}catch (InputMismatchException e) {
 			System.out.println("!!Valor invalido!!");
+			back();
 		}catch (NoSuchElementException e) {
 			System.out.println("!!!Valor não encontrado!!!");
 		}
